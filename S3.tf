f87e4b4d64c4e87d125e0f20b2f196bb35b2626c
Igor Gallo De Almeida <srheadshoot@outlook.com>
2022-09-10 15:51:04 -0300

    Atualização do S3.tf

--- S3.tf
+++ S3.tf
@@ -3,9 +3,21 @@ resource "aws_s3_bucket" "s3-top-top-top" {
   bucket = "s3-top-top-top"
 }
 
-resource "aws_s3_bucket_acl" "s3-top-top-top-acl" {
-  bucket = aws_s3_bucket.s3-top-top-top.id
-  acl    = "public-read"
+# POLICY S3
+resource "aws_s3_bucket_policy" "s3-top-top-top-policy" {
+  bucket = aws_s3_bucket.s3-top-top-top
+
+  policy      = jsonencode({
+    Version   = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow",
+        Principal = "*",
+        Action    = "s3:GetObject",
+        Resource  = "arn:aws:s3:::s3-top-top-top/*",
+      }
+    ]
+	})
 }
 
 resource "aws_s3_bucket_versioning" "s3-top-top-top-versioning" {
